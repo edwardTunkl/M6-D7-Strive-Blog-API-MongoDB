3a27d73ea34b48043e629b51e11c8bcbcb8322dd
edwardTunkl <eddytunkl@googlemail.com>
2021-11-03 16:43:59 +0100

    added token

--- src/services/authentication/basic.js
+++ src/services/authentication/basic.js
@@ -1,7 +1,7 @@
 import atob from "atob"
 import createHttpError from "http-errors"
-
 import AuthorModel from '../authors/schema.js'
+
 
 export const AuthorizationMiddleware = async (req, res, next) => {
   console.log("THIS IS REQ HEADERS", req.headers)
@@ -9,13 +9,14 @@ export const AuthorizationMiddleware = async (req, res, next) => {
   if (!req.headers.authorization) {
     next(createHttpError(401, "Please provide credentials in Authorization header"))
   } else {
+    
     const decodedCredents = atob(req.headers.authorization.split(" ")[1])
     console.log(decodedCredents)
 
     const [email, password] = decodedCredents.split(":")
     console.log("THIS IS EMAIL ", email)
     console.log("THIS IS PASSWORD ", password)
-
+    
     const user = await AuthorModel.checkCredentials(email, password)
     if (user) {
                                 // if the credentials were ok => ....another middleware, route handler, etc
--- src/services/authors/index.js
+++ src/services/authors/index.js
@@ -1,106 +1,145 @@
-import express from "express"
-import q2m from "query-to-mongo" // Translates queries in browser into mongo readable queries (F**k yeah!!!)
+import express from "express";
+import q2m from "query-to-mongo"; // Translates queries in browser into mongo readable queries (F**k yeah!!!)
 
-import AuthorModel from './schema.js'
-import BlogPostModel from './schema.js'
-import { AuthorizationMiddleware } from "../authentication/basic.js"
+import AuthorModel from "./schema.js";
+import BlogPostModel from "./schema.js";
+import { AuthorizationMiddleware } from "../authentication/basic.js";
+import { JWTAuthenticate } from "../authentication/token.js";
+import createHttpError from "http-errors";
 
-const authorsRouter = express.Router()
+const authorsRouter = express.Router();
 
+//---Post(/login)---
+
+authorsRouter.post("/login", AuthorizationMiddleware, async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    // veryfiy creds
+    const user = await AuthorModel.checkCredentials(email, password);
+
+    if (user) {
+
+      const accessToken = await JWTAuthenticate(user);
+      res.send({ accessToken });
+
+    } else {
+      next(createHttpError(401, "GO HOME!!!"));
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
 //---Get(me/stories)---
 
-authorsRouter.get("/me/stories", AuthorizationMiddleware, async (req, res, next) =>{
-try {
-  const posts = await BlogPostModel.find({author: req.user._id.toString()})
-  res.status(200).send(posts)
-} catch (error) {
-  next(error)
-}
-
-})
-
+authorsRouter.get(
+  "/me/stories",
+  AuthorizationMiddleware,
+  async (req, res, next) => {
+    try {
+      const posts = await BlogPostModel.find({
+        author: req.user._id.toString(),
+      });
+      // console.log(req.user)
+      res.status(200).send(posts);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 //---Get---
 
-authorsRouter.get("/", async(req, res, next) =>{
+authorsRouter.get("/", async (req, res, next) => {
   try {
-    const newQuery = q2m(req.query)
-    const total = await AuthorModel.countDocuments(newQuery.criteria)
-    const authors = await AuthorModel.find(newQuery.criteria, newQuery.options.fields)
+    const newQuery = q2m(req.query);
+    const total = await AuthorModel.countDocuments(newQuery.criteria);
+    const authors = await AuthorModel.find(
+      newQuery.criteria,
+      newQuery.options.fields
+    )
       .limit(newQuery.options.limit || 20)
       .skip(newQuery.options.skip)
-      .sort(newQuery.options.sort) // no matter how I write them but Mongo will always apply SORT then SKIP then LIMIT in this order
-    res.send({ links: newQuery.links("/authors", total), total, pageTotal: Math.ceil(total / newQuery.options.limit), authors })
+      .sort(newQuery.options.sort); // no matter how I write them but Mongo will always apply SORT then SKIP then LIMIT in this order
+    res.send({
+      links: newQuery.links("/authors", total),
+      total,
+      pageTotal: Math.ceil(total / newQuery.options.limit),
+      authors,
+    });
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
 //---Get:id---
 
 authorsRouter.get("/:authorId", async (req, res, next) => {
   try {
-    const authorId = req.params.authorId
-    const author = await AuthorModel.findById(authorId) // expects query as parameter
+    const authorId = req.params.authorId;
+    const author = await AuthorModel.findById(authorId); // expects query as parameter
 
     if (author) {
-      res.send(author)
+      res.send(author);
     } else {
-      next(createHttpError(404, `No Author with id ${authorId} found!`))
+      next(createHttpError(404, `No Author with id ${authorId} found!`));
     }
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
 //---Post---
 
-authorsRouter.post("/", async (req, res, next) => {
+authorsRouter.post("/register", async (req, res, next) => {
   try {
-    const newAuthor = new AuthorModel(req.body) // validation of the req.body
-    const { _id } = await newAuthor.save() // interaction with the db/collection
+    const newAuthor = new AuthorModel(req.body); // validation of the req.body
+    const { _id } = await newAuthor.save(); // interaction with the db/collection
 
-    res.status(201).send({ _id })
+    res.status(201).send({ _id });
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
 //---Put---
 
 authorsRouter.put("/:authorId", async (req, res, next) => {
   try {
-    const authorId = req.params.authorId
-    const modifiedAuthor = await AuthorModel.findByIdAndUpdate(authorId, req.body, {
-      new: true, // returns the modified Author
-    })
+    const authorId = req.params.authorId;
+    const modifiedAuthor = await AuthorModel.findByIdAndUpdate(
+      authorId,
+      req.body,
+      {
+        new: true, // returns the modified Author
+      }
+    );
 
     if (modifiedAuthor) {
-      res.send(modifiedAuthor)
+      res.send(modifiedAuthor);
     } else {
-      next(createHttpError(404, `No Author with id ${authorId} found!`))
+      next(createHttpError(404, `No Author with id ${authorId} found!`));
     }
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
 //---Delete---
 
 authorsRouter.delete("/:authorId", async (req, res, next) => {
   try {
-    const authorId = req.params.authorId
-    const deletedAuthor = await AuthorModel.findByIdAndDelete(authorId)
+    const authorId = req.params.authorId;
+    const deletedAuthor = await AuthorModel.findByIdAndDelete(authorId);
 
     if (deletedAuthor) {
-      res.status(204).send()
+      res.status(204).send();
     } else {
-      next(createHttpError(404, `No Author with id ${authorId} found!`))
+      next(createHttpError(404, `No Author with id ${authorId} found!`));
     }
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
-export default authorsRouter
+export default authorsRouter;
